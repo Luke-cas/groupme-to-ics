e874dae30cdbcbbb4b086a3e9ba7b02a7b10c1b4
Luke-cas <80925832+Luke-cas@users.noreply.github.com>
2023-10-12 16:01:55 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# GroupMe Calendar to ICS
+
+Turn your GroupMe event calendar into an ICS feed (for Google Calendar, Apple Calendar, Outlook, etc.).
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/amussey/groupme-calendar-to-ics)
+
+## Deploying
+
+When you set up this app, you'll be required to configure five environment variables:
+
+ * `GROUPME_GROUP_ID` - The ID for the GroupMe group.  This can be found on the webapp (https://web.groupme.com) under the Settings option for your group.
+ * `GROUPME_API_KEY` - A [GroupMe Developer Access Token](https://dev.groupme.com/docs/v3) for a user in the GroupMe group.
+ * `CACHE_DURATION` - The duration for which the GroupMe calendar is cached.  `0` will disable caching.
+ * `GROUPME_CALENDAR_TIMEZONE` - Timezone of the calendar events.  See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for list. Default is `America/Los_Angeles`
+ * `GROUPME_STATIC_NAME` - *(Optional)* A static name for the group.  This will lock a name for the calendar, even if the group decides to change their name.
+ * `GROUPME_PROXY_URL` - *(Optional)* A proxy URL to provide for the calendar.ics.
+
+
+### Default Settings
+
+ * **Cache Rate** - By default, the GroupMe calendar is refreshed and cached by one request every 60 minutes.  This is designed to help rate limit GroupMe API requests.  The caching rate can be modified or disabled by setting it to `0`.
---
+++ utils.py
@@ -0,0 +1,123 @@
+from icalendar import Calendar, Event
+from flask import Response
+import dateutil.parser
+import requests
+import urllib.parse
+import urllib
+from flask import current_app
+
+
+def return_ics_Response(response_body):
+    return Response(
+        response_body,
+        mimetype='text/calendar',
+        headers={'Content-Disposition': 'attachment'}
+    )
+
+
+def build_ics_urls(ics_url):
+    google_calendar_url_base = 'http://www.google.com/calendar/render?cid='
+
+    # Parse the URL into [scheme, netloc, path, params, query, fragment]
+    parsed_ics_url = list(urllib.parse.urlparse(ics_url))
+    if parsed_ics_url[0] != 'https':
+        parsed_ics_url[0] = 'http'
+    ics_url_http = urllib.parse.urlunparse(parsed_ics_url)
+
+    parsed_ics_url[0] = 'webcal'
+    ics_url_webcal = urllib.parse.urlunparse(parsed_ics_url)
+
+    parsed_google_url = list(urllib.parse.urlparse(google_calendar_url_base))
+    parsed_google_url[4] = dict(urllib.parse.parse_qsl(parsed_google_url[4]))
+    parsed_google_url[4]['cid'] = ics_url_webcal
+    parsed_google_url[4] = urllib.urlencode(parsed_google_url[4])
+    ics_url_google = urllib.parse.urlparse(parsed_google_url)
+
+    return ics_url_http, ics_url_webcal, ics_url_google
+
+
+def load_groupme_json(app, groupme_api_key, groupme_group_id):
+    url_group_info = 'https://api.groupme.com/v3/groups/{groupme_group_id}'.format(groupme_group_id=groupme_group_id)
+    url_calendar = 'https://api.groupme.com/v3/conversations/{groupme_group_id}/events/list'.format(groupme_group_id=groupme_group_id)
+    headers = {'X-Access-Token': groupme_api_key}
+
+    response = requests.get(url_calendar, headers=headers)
+    if response.status_code != 200:
+        current_app.groupme_load_successfully = False
+        current_app.groupme_calendar_json_cache = {}
+        app.logger.error('{}: {}'.format(response.status_code, response.text))
+        return False
+
+    current_app.groupme_calendar_json_cache = response.json()
+
+    response = requests.get(url_group_info, headers=headers)
+    if response.status_code == 200:
+        if response.json().get('response', {}).get('name', None):
+            current_app.groupme_calendar_name = response.json().get('response', {}).get('name')
+
+    current_app.groupme_load_successfully = True
+    return True
+
+
+def groupme_json_to_ics(groupme_json, static_name=None):
+    cal = Calendar()
+    cal['prodid'] = '-//Andrew Mussey//GroupMe-to-ICS 0.1//EN'
+    cal['version'] = '2.0'
+    cal['calscale'] = 'GREGORIAN'
+    cal['method'] = 'PUBLISH'
+    cal['x-wr-calname'] = 'GroupMe: {}'.format(current_app.groupme_calendar_name)
+    cal['x-wr-timezone'] = current_app.calendar_timezone
+
+    for json_blob in groupme_json['response']['events']:
+        if 'deleted_at' not in json_blob:
+            event = Event()
+            event['uid'] = json_blob['event_id']
+            event.add('dtstart', dateutil.parser.parse(json_blob['start_at']))
+            if json_blob.get('end_at'):
+                event.add('dtend', dateutil.parser.parse(json_blob['end_at']))
+            event['summary'] = json_blob['name']
+            event['description'] = json_blob.get('description', '')
+            if json_blob.get('location'):
+                location = json_blob.get('location', {})
+
+                if json_blob.get('description'):
+                    event['description'] += '\n\n'
+                event['description'] += 'Location:\n'
+
+                if location.get('name') and location.get('address'):
+                    event['location'] = "{}, {}".format(location.get('name'), location.get('address').strip().replace("\n", ", "))
+                    event['description'] += location.get('name')
+                    event['description'] += '\n'
+                    event['description'] += location.get('address')
+                elif location.get('name'):
+                    event['location'] = location.get('name')
+                    event['description'] += location.get('name')
+                elif location.get('address'):
+                    event['location'] = location.get('address').strip().replace("\n", ", ")
+                    event['description'] += location.get('address')
+
+                if location.get('lat') and location.get('lng'):
+                    location_url = 'https://www.google.com/maps?q={},{}'.format(location.get('lat'), location.get('lng'))
+                    if not event.get('location'):
+                        event['location'] = location_url
+                    else:
+                        event['description'] += '\n'
+                    event['description'] += location_url
+
+            if json_blob.get('updated_at'):
+                event['last-modified'] = dateutil.parser.parse(json_blob.get('updated_at'))
+            cal.add_component(event)
+
+    return cal.to_ical()
+
+
+def groupme_ics_error(error_text, static_name=None):
+    cal = Calendar()
+    cal['prodid'] = '-//Andrew Mussey//GroupMe-to-ICS 0.1//EN'
+    cal['version'] = '2.0'
+    cal['calscale'] = 'GREGORIAN'
+    cal['method'] = 'PUBLISH'
+    cal['x-wr-calname'] = 'GroupMe: {} ({})'.format(current_app.groupme_calendar_name, error_text)
+    cal['x-wr-timezone'] = 'America/Los_Angeles'
+
+    return cal.to_ical()
