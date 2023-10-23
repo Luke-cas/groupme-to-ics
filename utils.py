1362e7843c360fc33d5ca0a6c22f4e8d307ee96a
Luke-cas <80925832+Luke-cas@users.noreply.github.com>
2023-10-23 14:22:22 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -60,4 +60,65 @@ def load_groupme_json(app, groupme_api_key, groupme_group_id):
     current_app.groupme_load_successfully = True
     return True
 
-# ... (rest of your functions like groupme_json_to_ics and groupme_ics_error remain the same)
+def groupme_json_to_ics(groupme_json, static_name=None):
+    cal = Calendar()
+    cal['prodid'] = '-//Andrew Mussey//GroupMe-to-ICS 0.1//EN'
+    cal['version'] = '2.0'
+    cal['calscale'] = 'GREGORIAN'
+    cal['method'] = 'PUBLISH'
+    cal['x-wr-calname'] = 'GroupMe: {}'.format(current_app.groupme_calendar_name)
+    cal['x-wr-timezone'] = current_app.calendar_timezone
+
+    for json_blob in groupme_json['response']['events']:
+        if 'deleted_at' not in json_blob:
+            event = Event()
+            event['uid'] = json_blob['event_id']
+            event.add('dtstart', dateutil.parser.parse(json_blob['start_at']))
+            if json_blob.get('end_at'):
+                event.add('dtend', dateutil.parser.parse(json_blob['end_at']))
+            event['summary'] = json_blob['name']
+            event['description'] = json_blob.get('description', '')
+            if json_blob.get('location'):
+                location = json_blob.get('location', {})
+
+                if json_blob.get('description'):
+                    event['description'] += '\n\n'
+                event['description'] += 'Location:\n'
+
+                if location.get('name') and location.get('address'):
+                    event['location'] = "{}, {}".format(location.get('name'), location.get('address').strip().replace("\n", ", "))
+                    event['description'] += location.get('name')
+                    event['description'] += '\n'
+                    event['description'] += location.get('address')
+                elif location.get('name'):
+                    event['location'] = location.get('name')
+                    event['description'] += location.get('name')
+                elif location.get('address'):
+                    event['location'] = location.get('address').strip().replace("\n", ", ")
+                    event['description'] += location.get('address')
+
+                if location.get('lat') and location.get('lng'):
+                    location_url = 'https://www.google.com/maps?q={},{}'.format(location.get('lat'), location.get('lng'))
+                    if not event.get('location'):
+                        event['location'] = location_url
+                    else:
+                        event['description'] += '\n'
+                    event['description'] += location_url
+
+            if json_blob.get('updated_at'):
+                event['last-modified'] = dateutil.parser.parse(json_blob.get('updated_at'))
+            cal.add_component(event)
+
+    return cal.to_ical()
+
+
+def groupme_ics_error(error_text, static_name=None):
+    cal = Calendar()
+    cal['prodid'] = '-//Andrew Mussey//GroupMe-to-ICS 0.1//EN'
+    cal['version'] = '2.0'
+    cal['calscale'] = 'GREGORIAN'
+    cal['method'] = 'PUBLISH'
+    cal['x-wr-calname'] = 'GroupMe: {} ({})'.format(current_app.groupme_calendar_name, error_text)
+    cal['x-wr-timezone'] = 'America/Los_Angeles'
+
+    return cal.to_ical()
